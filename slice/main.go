1effd5c2fe9315a6af033de3c4ca4ea1a2c09864
Nick Boughton <nicholasboughton@gmail.com>
2018-07-23 10:11:56 +0100

    fixored

--- slice/main.go
+++ slice/main.go
@@ -11,7 +11,7 @@ var (
 
 func main() {
 	set := []int{0, 0, 0, 0, 0, 0, 0}
-	//ans := 1
+	ans := 1
 	for set[0] = 0; sum(set[:1]) <= lim; set[0]++ { // 1s
 		for set[1] = 0; sum(set[:2]) <= lim; set[1] += 2 { // 2s
 			for set[2] = 0; sum(set[:3]) <= lim; set[2] += 5 { // 5s
@@ -19,13 +19,11 @@ func main() {
 					for set[4] = 0; sum(set[:5]) <= lim; set[4] += 20 { // 20s
 						for set[5] = 0; sum(set[:6]) <= lim; set[5] += 50 { // 50s
 							for set[6] = 0; sum(set) <= lim; set[6] += 100 { // 100s
-								fmt.Println(set)
-								/*
-									if sum(set) == lim {
-										ans++
-										fmt.Printf("%-3v %v\n", set, ans)
-									}
-								*/
+								//fmt.Println(set)
+								if sum(set) == lim {
+									ans++
+									fmt.Printf("%-3v %v\n", set, ans)
+								}
 							}
 						}
 					}
