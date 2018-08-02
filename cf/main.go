5931b8953e014f504971c4791d214ae9d31a27ec
Nick Boughton <nicholasboughton@gmail.com>
2018-08-02 01:06:38 +0100

    now that you have the CF you need to calculate the convergents

--- cf/main.go
+++ cf/main.go
@@ -11,7 +11,24 @@ import (
 var l = []float64{0.245, 0.045, 1.56, 0.75}
 
 func main() {
-	for n := range num.ContinuedFraction(new(big.Rat).SetFloat64(math.Sqrt2)) {
-		fmt.Println(n)
+	for d := 3.; d <= 1000; d++ {
+		f := math.Sqrt(d)
+		if _, frac := math.Modf(f); frac == 0 {
+			continue
+		}
+
+		for n := range num.ContinuedFraction(new(big.Rat).SetFloat64(f)) {
+			//fmt.Println(n.Frac.Num(), big.NewInt(int64(d)), n.Frac.Denom(), diophantine(n.Frac.Denom(), big.NewInt(int64(d)), n.Frac.Num()))
+			fmt.Println(n)
+		}
+		break
 	}
+}
+
+func diophantine(x, d, y *big.Int) *big.Int {
+	x2 := new(big.Int).Mul(x, x)
+	y2 := new(big.Int).Mul(y, y)
+	Dy2 := new(big.Int).Mul(d, y2)
+
+	return new(big.Int).Sub(x2, Dy2)
 }
