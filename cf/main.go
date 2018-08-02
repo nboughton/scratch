3a6f4462694c9d2bc6f0c929774111b65391242a
Nick Boughton <nicholasboughton@gmail.com>
2018-08-02 09:56:52 +0100

    hmmm

--- cf/main.go
+++ cf/main.go
@@ -11,21 +11,22 @@ import (
 var l = []float64{0.245, 0.045, 1.56, 0.75}
 
 func main() {
-	for d := 3.; d <= 1000; d++ {
+	for d := 2.; d <= 10; d++ {
 		f := math.Sqrt(d)
 		if _, frac := math.Modf(f); frac == 0 {
 			continue
 		}
 
 		for n := range num.ContinuedFraction(new(big.Rat).SetFloat64(f)) {
-			//fmt.Println(n.Frac.Num(), big.NewInt(int64(d)), n.Frac.Denom(), diophantine(n.Frac.Denom(), big.NewInt(int64(d)), n.Frac.Num()))
-			fmt.Println(n)
+			x, y := new(big.Int).Set(n.Frac.Num()), new(big.Int).Set(n.Frac.Denom())
+			//if diophantine(x, big.NewInt(int64(d)), y).Cmp(big.NewInt(1)) == 0 {
+			fmt.Println(d, n, x, y, diophantine(x, big.NewInt(int64(d)), y))
+			//}
 		}
-		break
 	}
 }
 
-func diophantine(x, d, y *big.Int) *big.Int {
+func diophantine(y, d, x *big.Int) *big.Int {
 	x2 := new(big.Int).Mul(x, x)
 	y2 := new(big.Int).Mul(y, y)
 	Dy2 := new(big.Int).Mul(d, y2)
